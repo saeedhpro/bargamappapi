78ea44127240d78be25dac7e1ec7885d97fd17b9
Saeed Heydari <saeedhpro@gmail.com>
2025-12-02 18:17:47 +0330

    add compress image and format jpeg on tools page

--- init_db.py
+++ init_db.py
@@ -1,27 +1,28 @@
 from tortoise import Tortoise
 
-from models.subscription import SubscriptionPlan
-
+# 1. تعریف تنظیمات در یک دیکشنری به نام TORTOISE_ORM
+TORTOISE_ORM = {
+    "connections": {
+        "default": "postgres://saeed:saeed%402311@localhost:5432/golbeendb"
+    },
+    "apps": {
+        "models": {
+            "models": [
+                "models.user",
+                "models.subscription",
+                "models.history",
+                "models.garden",
+                "aerich.models"
+            ],
+            "default_connection": "default",
+        }
+    },
+}
 
 async def init_db():
-    await Tortoise.init(
-        db_url="postgres://saeed:saeed%402311@localhost:5432/golbeendb",
-        modules={"models": ["models.user", "models.subscription", "models.history", "models.garden"]}
-    )
+    await Tortoise.init(config=TORTOISE_ORM)
     await Tortoise.generate_schemas()
 
 
 async def init_db_data():
     pass
-    # default_exists = await SubscriptionPlan.filter(is_default=True).exists()
-    # if not default_exists:
-    #     await SubscriptionPlan.create(
-    #         title="اشتراک رایگان",
-    #         description="شامل استفاده محدود روزانه",
-    #         price=0,
-    #         daily_plant_id_limit=1,
-    #         daily_disease_id_limit=1,
-    #         is_default=True,
-    #         is_active=True
-    #     )
-    #     print("Default free plan created.")
